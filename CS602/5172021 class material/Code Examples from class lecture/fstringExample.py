a95410513896f9bfbd0815cfba669f3c042d1300
omeletteYAN <71146842+omeletteYAN@users.noreply.github.com>
2021-07-14 00:12:49 -0400

    Delete CS602 directory

--- CS602/5172021 class material/Code Examples from class lecture/fstringExample.py
+++
@@ -1,4 +0,0 @@
-number = 1000000
-
-print(f'The number, 1000000, formatted with a comma {number:,.2f}')
-print(f'The number, 1000000, formatted with a comma and rightaligned in a width of 15 {number:>15,.2f}')
