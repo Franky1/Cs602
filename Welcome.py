f025d8398df32c51b8d3b3da65aee48a33fd0c24
Qiaofei-Y <71146842+Qiaofei-Y@users.noreply.github.com>
2022-06-21 12:13:00 -0400

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -11,32 +11,7 @@ def app():
        
     #Welcome sidebar
     st.sidebar.write("Hi!")
-    container = st.sidebar.container()
-    
-    #set time.sleep
-    for percent_complete in range(1):
-        time.sleep(1)
-        container.write(
-            "Welcome to my homepage. ")
-        time.sleep(1)
-        container.write(
-            "I hope you like it")
-        time.sleep(1)
-    st.sidebar.write("Welcome to ask questions💡")
-    time.sleep(1)
-    
-    st.sidebar.markdown('***')
-    
-    st.sidebar.subheader(
-        """Description: 
-        You can choose Two pages
-        select the main page. 
-        you will see the information
-        about the volcanoe you select,
-        also map will show the volcanoe""")
-    
-    st.sidebar.markdown('***')
-    
+
     st.sidebar.subheader("""
     Made by:[Qiaofei Yan](https://omeletteyan.github.io/index/)
     Summer 2021 Cs602 Class                                                                     
