a34f2dc131b6a8f05800e77d6aa68fd1dc119775
Qiaofei-Y <71146842+Qiaofei-Y@users.noreply.github.com>
2022-06-21 12:16:52 -0400

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -8,20 +8,14 @@ import pandas as pd
 
 #connect to page.py
 def app():
-       def Volcanoes_load(url: str):
-        r = requests.get(url)
-        if r.status_code != 200:
-            return None
-        return r.json()
-           load_volcanoes = Volcanoes_load('https://assets6.lottiefiles.com/packages/lf20_ZdVYgO.json')#https://assets4.lottiefiles.com/packages/lf20_xCfcGS.json
-           #Welcome sidebar
-           st.sidebar.write("Hi!")
+    #Welcome sidebar
+    st.sidebar.write("Hi!")
 
-           st.sidebar.subheader("""
-           Made by:[Qiaofei Yan](https://omeletteyan.github.io/index/)
-           Summer 2021 Cs602 Class                                                                     
-           Data: [Volcanoes.csv](https://github.com/omeletteYAN/streamlit/blob/main/volcanoes.csv)  datasets                                                     
-           Source Web: [volcano.com](https://volcano.si.edu/)""")
+    st.sidebar.subheader("""
+    Made by:[Qiaofei Yan](https://omeletteyan.github.io/index/)
+    Summer 2021 Cs602 Class                                                                     
+    Data: [Volcanoes.csv](https://github.com/omeletteYAN/streamlit/blob/main/volcanoes.csv)  datasets                                                     
+    Source Web: [volcano.com](https://volcano.si.edu/)""")
 
     
     
